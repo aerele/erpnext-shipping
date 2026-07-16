6637be3fa56a7292eaf6e5f70275491104c1dda5
sarathibalamurugan <sarathigreen4@gmail.com>
2026-07-16 13:13:57 +0530

    fix: remove unused imports and variable for delhiveryone

--- erpnext_shipping/erpnext_shipping/doctype/envia/constants.py
+++
@@ -1,4 +0,0 @@
-TEST_BASE_URL_API = "https://api-test.envia.com"
-TEST_BASE_URL_QUERY = "https://queries-test.envia.com"
-BASE_URL_API = "https://api.envia.com"
-BASE_URL_QUERY = "https://queries.envia.com"
